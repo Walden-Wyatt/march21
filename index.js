dfd3730aa3f5854a95242b3d814295d3aa27998b
unknown <investob848@gmail.com>
2023-03-21 12:55:22 +0530

    version 5

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+ const donutResponce = confirm( "Do you wants donuts" );
+
+ if ( donutResponce ) {
+
+  alert( "Here is your strawberry flovour donuts" );
+
+ }
+
+ else {
+
+   alert( "Ok you can eat some other things" )
+
+ }
+
+
+
